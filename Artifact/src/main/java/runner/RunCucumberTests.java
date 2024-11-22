a9942556f731cf3f547586a7912cf2131a8ed771
Prasad <narayana23prasad@gmail.com>
2024-11-22 13:52:10 +0530

    Upadted comments in runner

--- Artifact/src/main/java/runner/RunCucumberTests.java
+++ Artifact/src/main/java/runner/RunCucumberTests.java
@@ -2,7 +2,7 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+//runapi is for api tests and //runuii is for api tests
 @CucumberOptions( dryRun = false,
 features = {"src/main/java/features"},
 glue = {"stepdefs","hooks"},
